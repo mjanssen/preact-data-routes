c850e0fe95f690757879dc71b49374dc9c3eaa6e
mjanssen <dev.marnix@gmail.com>
2018-05-07 23:25:25 +0200

    Updated render to DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import './css/style';
+import './css/style.css';
+import { render, h } from 'preact';
 import AppContainer from './containers/AppContainer';
+window.h = h;
+render(<AppContainer />, document.body);
 
-export default function App() {
-  return <AppContainer />;
-}
+// export default function App() {
+//   return <AppContainer />;
+// }
